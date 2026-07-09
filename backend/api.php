f7ac40a39c0be470295f3f5624a52bb12da508ba
Karam Alatwan <karam.samer50@gmail.com>
2026-07-09 22:16:03 +0300

    Add files via upload

---
+++ backend/api.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Access-Control-Allow-Credentials: true");
+
+echo json_encode([
+    'old' => $_SESSION['old'] ?? [],
+    'errors' => $_SESSION['errors'] ?? [],
+]);
+
+unset($_SESSION['errors']);
+
+exit;
---
+++ backend/variable.php
@@ -0,0 +1,19 @@
+<?php
+$errors = [];
+$old = [];
+$values = ['Name', 'Email', 'Language', 'Password', 'CPassword'];
+$regex=array(
+    'Name' => '/^[A-Za-z\s]+$/',
+    'Email' => '/^[A-Za-z]+[A-Za-z0-9\-.]+@(gmail|yahoo|outlook)\.(com|org|net)$/',
+    'Language' => '/^(English|French|Spanish)$/',
+    'Password' => '/^[\w!@]{8,20}$/',
+    'CPassword' => '/^[\w!@]{8,20}$/'
+);
+$regexError = array(
+    'Name' => '* Name must contain only letters and spaces',
+    'Email' => '* Email must be a valid email address (gmail, yahoo, outlook)',
+    'Language' => '* Language must be either English, French, or Spanish',
+    'Password' => '* Password must be 8-20 characters',
+    'CPassword' => '* Confirm Password must identical to Password'
+);
+    $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
