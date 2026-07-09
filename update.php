3696f1bd961c42cc54e3b899f84e06e2465cacdc
Karam Alatwan <karam.samer50@gmail.com>
2026-07-09 22:34:14 +0300

    Add files via upload

--- update.php
+++ update.php
@@ -5,7 +5,6 @@ session_start();
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: http://localhost:4200");
 header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
